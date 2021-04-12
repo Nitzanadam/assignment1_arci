e28937f9dd269add77a55a09c7b84947c25180eb
Nitzanadam <nitzanadam40@gmail.com>
2021-04-12 21:13:26 +0300

    Nitzanadam

--- asm_task1Assignment1.s
+++ asm_task1Assignment1.s
@@ -14,17 +14,13 @@ assFunc:
     push   ebp               ;establishing stack-frame
     mov ebp, esp             ;reset EBP to the current ESP
     pushad                   ;backup registers
-    ;check if we need to push here the arguments of the function 
-    mov eax, [ebp+4]        ;get function argument on stack
-    call c_checkValidity    ;call function to check if the number is even 
-    mov [result], eax       ;save the function result to result
-    
-
-
-    add esp, 4              ;"free" space allocated for function arguments in stack (free x)
+    mov eax, [ebp+4]         ;get function argument on stack
+    call c_checkValidity     ;call function to check if the number is even 
+    mov [result], eax        ;save the function result to result
+    add esp, 4               ;"free" space allocated for function arguments in stack (free x)
     cmp dword[result], 1     ;compare between result to 1
-    JE is_One               ;if is equel go to is_one
-    JNE is_Zero             ; is is not equal go to is_zero
+    JE is_One                ;if is equel go to is_one
+    JNE is_Zero              ; is is not equal go to is_zero
 endOfFunc:
     push z
     push format_string
