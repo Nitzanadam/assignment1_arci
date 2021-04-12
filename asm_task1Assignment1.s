c05b5410c2471cbc85ca68e3c262ad7f4ffb379f
Nitzanadam <nitzanadam40@gmail.com>
2021-04-12 19:20:09 +0300

    running , now we need logic

--- asm_task1Assignment1.s
+++ asm_task1Assignment1.s
@@ -1,48 +1,51 @@
-extern c_checkValidity      ;define as en external function
-
 section	.rodata			    ; we define (global) read-only variables in .rodata section
 	format_string: db "%d", 10, 0	; format string
 
-section .bss                ;we define (global) initialized variables in .data section
-    result resq 1  
-    z resq 1
+section .bss                  ;we define (global) initialized variables in .data section
+    result resq 1             ;we allocated place of 8 byte(resq) to result
+    z resq 1                  ;we allocated place of 8 byte to z  
 
-section .text               ;we write :code in .text section 
-    global start   
-    global assFunc 
-    extern printf
+section .text                 ;we write code in .text section 
+     global assFunc 
+     extern printf
+     extern c_checkValidity   ;define as en external function
+
+assFunc:                                             
+    push   ebp               ;establishing stack-frame
+    mov ebp, esp             ;reset EBP to the current ESP
+    pushad                   ;backup registers
+    ;check if we need to push here the arguments of the function 
+    mov eax, [ebp+4]        ;get function argument on stack
+    call c_checkValidity    ;call function to check if the number is even 
+    mov [result], eax       ;save the function result to result
     
 
-assFunc: 
-    pushad                  ;backup registers
-    pushfd                  ;backup EFLAGS  
-;    push ebp                ;establishing stack-frame
-;    mov ebp, esp             ;reset EBP to the current ESP
-;    mov eax, [ebp+4]        ;get function argument on stack
-;    call c_checkValidity    ;call function to check if the number is even 
-;    mov [result], eax       ;save the function result to result
-;    add esp, 4              ;"free" space allocated for function arguments in stack (free x)
-;    cmp byte[result], 1         ;compare between result to 1
-;    JE is_One               ;if is equel go to is_one
-;    JNE is_Zero             ; is is not equal go to is_zero
 
-;    push z
-;    push format_string
-;    call printf
-;    add esp, 8               ; clean up stack after call
-
-is_One:
-    shl byte[result], 2
-    mov dword[z], result
-
-is_Zero:
-    shl byte[result], 3
-    mov dword[z], result
-
-
+    add esp, 4              ;"free" space allocated for function arguments in stack (free x)
+    cmp dword[result], 1     ;compare between result to 1
+    JE is_One               ;if is equel go to is_one
+    JNE is_Zero             ; is is not equal go to is_zero
+endOfFunc:
+    push z
+    push format_string
+    call printf
+    add esp, 8               ; clean up stack after call
     popad			
 	mov esp, ebp	
 	pop ebp
 	ret
+
+is_One:
+    shl dword[result], 2
+    mov dword[z], result
+    jmp endOfFunc
+
+is_Zero:
+    shl dword[result], 3
+    mov dword[z], result
+    jmp endOfFunc
+
+
+  
 
 
